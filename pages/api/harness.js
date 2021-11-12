bae4d43b434810fde9b15eaa4653b781636f470c
Alfredo Bonilla <brolag@live.com>
2021-11-12 17:28:02 -0600

    feat: add ipsn request integration, wip

--- pages/api/harness.js
+++ pages/api/harness.js
@@ -1,5 +1,6 @@
 import { Web3Storage } from 'web3.storage'
 import { File } from 'web3.storage'
+import { create } from 'ipfs-http-client'
 
 function getAccessToken() {
   return process.env.WEB3STORAGE_TOKEN
@@ -7,6 +8,18 @@ function getAccessToken() {
 
 function makeStorageClient() {
   return new Web3Storage({ token: getAccessToken() })
+}
+
+function makeIpfsClient() {
+  return create()
+}
+
+async function assignIpsn(cid) {
+  const client = makeIpfsClient()
+  const addr = `/ipfs/${cid}`
+  const ipsn = await client.name.publish(addr)
+
+  return ipsn
 }
 
 function makeFileObjects(data) {
@@ -29,6 +42,7 @@ export default async (req, res) => {
   if (req.method === 'POST') {
     const files = makeFileObjects(req.body)
     const cid = await storeFiles(files)
+    // const ipsn = await assignIpsn(cid)
     res.statusCode = 200
     res.json({ cid })
   }
