dd5ba9e6fff334f68e1269867cd5bdc3d697a023
Alfredo Bonilla <brolag@live.com>
2021-11-15 16:14:12 -0600

    fix: add error handling

--- pages/api/coordinape.js
+++ pages/api/coordinape.js
@@ -2,19 +2,18 @@ import { makeFileObjects, storeFiles } from '../../util/web3Storage'
 import { mapCoordinapeData } from '../../util/coordinape'
 
 export default async (req, res) => {
-  //try {
-  if (req.method === 'POST') {
-    console.log('req.body', req.body)
-    const coordinapeData = mapCoordinapeData(req.body.coordinape)
-    const files = makeFileObjects(coordinapeData)
-    const cid = await storeFiles(files)
+  try {
+    if (req.method === 'POST') {
+      console.log('req.body', req.body)
+      const coordinapeData = mapCoordinapeData(req.body.coordinape)
+      const files = makeFileObjects(coordinapeData)
+      const cid = await storeFiles(files)
 
-    res.statusCode = 200
-    console.log('cid', cid)
-    res.json({ cid })
+      res.statusCode = 200
+      console.log('cid', cid)
+      res.json({ cid })
+    }
+  } catch (e) {
+    res.status(422).send(e)
   }
-  // } catch (e) {
-  //   // Unprocessable Entity
-  //   res.status(422).send(e)
-  // }
 }
