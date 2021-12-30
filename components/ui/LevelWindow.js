c0c95fbb12a6b1be69d46a3cc1caf42b848ba7ef
David McKelvey <david@mckelveycreative.co>
2021-12-29 16:15:22 -0800

    add scroll buttons

--- components/ui/LevelWindow.js
+++ components/ui/LevelWindow.js
@@ -76,15 +76,39 @@ const TitleBarButton = styled.button`
   }
 `
 
+const sharedScrollButtonStyles = css`
+  display: block;
+  width: inherit;
+  height: 4.4rem;
+  background-color: ${props => props.theme.colors.vibrantBlack};
+  background-repeat: no-repeat, no-repeat;
+`
+
+const sharedScrollButtonHoverStyles = css`
+  background-color: ${props => props.theme.colors.mutedBlack};
+`
+
+const sharedStartScrollButtonStyles = css`
+  ${sharedScrollButtonStyles}
+  background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAAAIAQAAAABGt0WaAAAAEUlEQVR4AWMIhQOGVXBAuSgAkMYn2b1UVnQAAAAASUVORK5CYII='), url('data:image/svg+xml;utf8,<svg fill="none" height="14" viewBox="0 0 19 14" width="19" xmlns="http://www.w3.org/2000/svg"><path d="m10.7163 1.53979 6.9155 9.58241c.716.992.0071 2.3778-1.2163 2.3778h-13.831c-1.22341 0-1.932276-1.3858-1.21633-2.3778l6.9155-9.58241c.59871-.829585 1.83393-.829585 2.43263 0z" fill="%23efecd3" stroke="%23efecd3"/></svg>');
+  background-position: left bottom, center calc(50% - 0.4rem);
+  background-size: 4rem 0.4rem, 1.9rem 1.4rem;
+`
+
+const sharedEndScrollButtonStyles = css`
+  ${sharedScrollButtonStyles}
+  background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAAAIAQAAAABGt0WaAAAAEUlEQVR4AWMIhQOGVXBAuSgAkMYn2b1UVnQAAAAASUVORK5CYII='), url('data:image/svg+xml;utf8,<svg fill="none" height="14" viewBox="0 0 19 14" width="19" xmlns="http://www.w3.org/2000/svg"><path d="m10.7163 12.4602c-.5987.8296-1.83392.8296-2.43263 0l-6.9155-9.58239c-.715943-.99205-.00708-2.377812 1.21633-2.377812l13.831.000002c1.2234 0 1.9323 1.38576 1.2163 2.37781z" fill="%23efecd3" stroke="%23efecd3"/></svg>');
+  background-position: left top, center calc(50% + 0.4rem);
+  background-size: 4rem 0.4rem, 1.9rem 1.4rem;
+`
+
 const Content = styled.div`
   height: auto;
   overflow-x: hidden;
   overflow-y: auto;
-  background-color: rebeccapurple;
   border-top-style: solid;
   border-top-color: ${props => props.theme.colors.vibrantBlack};
   border-bottom-left-radius: 1.05rem;
-  transition: max-height 233ms ease, border-top-width 233ms ease;
   transition-origin: center top;
 
   ${({ isCollapsed, maxHeight }) =>
@@ -92,11 +116,14 @@ const Content = styled.div`
       ? css`
           max-height: 0;
           border-top-width: 0;
+          transition: max-height 233ms ease, border-top-width 34ms ease 233ms;
         `
       : css`
           max-height: calc(${maxHeight} - 6.6rem - 0.4rem * 2);
           border-top-width: 0.4rem;
-        `}
+          transition: max-height 233ms ease 34ms, border-top-width 34ms ease;
+        `
+  }
 
   ::-webkit-scrollbar {
     width: 40px;
@@ -117,6 +144,22 @@ const Content = styled.div`
 
   ::-webkit-scrollbar-thumb:active {
     background-color: ${props => props.theme.colors.mutedBlack};
+  }
+
+  ::-webkit-scrollbar-button:vertical:start:decrement {
+    ${sharedStartScrollButtonStyles}
+  }
+
+  ::-webkit-scrollbar-button:vertical:start:decrement:hover {
+    ${sharedScrollButtonHoverStyles}
+  }
+
+  ::-webkit-scrollbar-button:vertical:end:increment {
+    ${sharedEndScrollButtonStyles}
+  }
+
+  ::-webkit-scrollbar-button:vertical:end:increment:hover {
+    ${sharedScrollButtonHoverStyles}
   }
 `
 
