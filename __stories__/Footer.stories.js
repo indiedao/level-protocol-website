1673f48c280fff934bed33cd077a8a02d46f9ccf
Mike Ackerman <mikeackerman@gmail.com>
2022-01-06 16:29:25 -0600

    Adds Footer Component & Storybook > Footer example

---
+++ __stories__/Footer.stories.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import FooterUI from '../components/ui/Footer'
+import StoryGrid from './StoryGrid'
+import theme from '../util/theme'
+
+const trueColors = Object.keys(theme.colors).filter(colorName =>
+  /^true/.test(colorName),
+)
+
+const vibrantColors = Object.keys(theme.colors).filter(colorName =>
+  /^vibrant/.test(colorName),
+)
+
+const mutedColors = Object.keys(theme.colors).filter(colorName =>
+  /^muted/.test(colorName),
+)
+
+const Template = ({ 'Background Color': backgroundColor }) => (
+  <StoryGrid columns={1}>
+    <FooterUI backgroundColor={theme.colors[backgroundColor]} />
+  </StoryGrid>
+)
+
+export const Footer = Template.bind({})
+Footer.args = {
+  'Background Color': `${theme.colors.vibrantBlack}`
+}
+
+const Story = {
+  title: 'Design System / Footer',
+  argTypes: {
+    'Background Color': {
+      options: [
+        ...trueColors,
+        ...vibrantColors,
+        ...mutedColors,
+        'black',
+        'white',
+      ],
+      control: {
+        type: 'select',
+      },
+    },
+  },
+}
+
+export default Story
---
+++ components/ui/Footer.js
@@ -0,0 +1,89 @@
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import Link from 'next/link'
+import { StyledLinkText, Body1 } from './Typography'
+import theme from '../../util/theme'
+
+const FooterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 8rem;
+
+  background: ${props => props.backgroundColor}
+    ${props => props.theme.halftones.md};
+`
+
+const FooterLinksContainer = styled.div`
+  display: flex;
+  font-size: 2rem;
+  line-height: 2rem;
+
+  > :not([hidden]) ~ :not([hidden]) {
+    border-right-width: 0;
+    border-left-width: 0.1rem;
+    border-left-style: solid;
+    border-color: ${theme.colors.white};
+  }
+`
+
+const FooterLink = styled.div`
+  padding: 0 1.2rem;
+`
+
+const Copyright = styled.div`
+  padding-top: 1.6rem;
+  color: ${theme.colors.white};
+`
+
+const Footer = ({ backgroundColor }) => (
+  <FooterContainer backgroundColor={backgroundColor}>
+    <FooterLinksContainer>
+      <FooterLink>
+        <Link href="#TODO">
+          <StyledLinkText>
+            Website
+          </StyledLinkText>
+        </Link>
+      </FooterLink>
+      <FooterLink>
+        <Link href="https://docs.google.com/document/d/1mv4vfrYRBwc8nI7jGBoqDITV-desH_UhFNA3UW8dUnw/edit#" target="_blank">
+          <StyledLinkText>
+            Whitepaper
+          </StyledLinkText>
+        </Link>
+      </FooterLink>
+      <FooterLink>
+        <Link href="#TODO">
+          <StyledLinkText>
+            Twitter
+          </StyledLinkText>
+        </Link>
+      </FooterLink>
+      <FooterLink>
+        <Link href="#TODO">
+          <StyledLinkText>
+            Discord
+          </StyledLinkText>
+        </Link>
+      </FooterLink>
+    </FooterLinksContainer>
+    <Copyright>
+      <Body1>
+        Copyright © {new Date().getFullYear()} The IndieDAO
+      </Body1>
+    </Copyright>
+  </FooterContainer>
+)
+
+Footer.propTypes = {
+  backgroundColor: PropTypes.string,
+}
+
+Footer.defaultProps = {
+  backgroundColor: `${theme.colors.vibrantBlack}`,
+}
+
+export default Footer
