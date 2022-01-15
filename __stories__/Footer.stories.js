333f05fb15123524b28056b7e3cadee39361e8c4
David McKelvey <david@mckelveycreative.co>
2022-01-15 15:43:18 -0800

    Footer updates

--- __stories__/Footer.stories.js
+++ __stories__/Footer.stories.js
@@ -1,55 +1,13 @@
-import React from 'react'
-import styled from 'styled-components'
-
 import FooterUI from '../components/ui/Footer'
-import StoryGrid from './StoryGrid'
-import theme from '../util/theme'
 
-const trueColors = Object.keys(theme.colors).filter(colorName =>
-  /^true/.test(colorName),
-)
-
-const vibrantColors = Object.keys(theme.colors).filter(colorName =>
-  /^vibrant/.test(colorName),
-)
-
-const mutedColors = Object.keys(theme.colors).filter(colorName =>
-  /^muted/.test(colorName),
-)
-
-const FooterBackground = styled.div`
-  background-color: ${props => props.backgroundColor};
-`
-
-const Template = ({ 'Background Color': backgroundColor }) => (
-  <StoryGrid columns={1}>
-    <FooterBackground backgroundColor={theme.colors[backgroundColor]}>
-      <FooterUI />
-    </FooterBackground>
-  </StoryGrid>
-)
+const Template = () => <FooterUI />
 
 export const Footer = Template.bind({})
-Footer.args = {
-  'Background Color': `${theme.colors.vibrantBlack}`,
-}
+Footer.args = {}
 
 const Story = {
-  title: 'Sections / Footer',
-  argTypes: {
-    'Background Color': {
-      options: [
-        ...trueColors,
-        ...vibrantColors,
-        ...mutedColors,
-        'black',
-        'white',
-      ],
-      control: {
-        type: 'select',
-      },
-    },
-  },
+  title: 'Marketing / Footer',
+  component: FooterUI,
 }
 
 export default Story
--- components/ui/Footer.js
+++ components/ui/Footer.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components'
 
-import { StyledLinkText, Body1 } from './Typography'
-import theme from '../../util/theme'
+import { Body1 } from './Typography'
+import Link from './Link'
 
 const FooterContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 8rem;
+  padding: 8rem 0;
 
   background: transparent ${props => props.theme.halftones.md};
 `
@@ -19,62 +19,48 @@ const FooterLinksContainer = styled.nav`
   line-height: 2rem;
 
   > *:nth-child(n + 2) {
-    border-right-width: 0;
+    margin-left: 1.2rem;
+    padding-left: 1.2rem;
     border-left-width: 0.1rem;
     border-left-style: solid;
-    border-color: ${theme.colors.white};
+    border-color: ${props => props.theme.colors.trueWhite};
   }
-`
-
-const A = styled.a`
-  padding: 0 1.2rem;
 `
 
 const Copyright = styled.div`
   padding-top: 1.6rem;
-  color: ${theme.colors.white};
+  color: ${props => props.theme.colors.trueWhite};
 `
 
 const Footer = () => (
   <FooterContainer>
     <FooterLinksContainer>
-      <A
+      <Link
         href="https://docs.google.com/document/d/1mv4vfrYRBwc8nI7jGBoqDITV-desH_UhFNA3UW8dUnw/edit#"
         target="_blank"
-        rel="noopener noreferrer"
+        rel="noopener"
       >
-        <StyledLinkText>Whitepaper</StyledLinkText>
-      </A>
-      <A
+        Whitepaper
+      </Link>
+      <Link
         href="https://twitter.com/lvlprotocol"
         target="_blank"
-        rel="noopener noreferrer"
+        rel="noopener"
       >
-        <StyledLinkText>Twitter</StyledLinkText>
-      </A>
-      <A
+        Twitter
+      </Link>
+      <Link
         href="https://twitter.com/theindiedao"
         target="_blank"
-        rel="noopener noreferrer"
+        rel="noopener"
       >
-        <StyledLinkText>IndieDAO</StyledLinkText>
-      </A>
-      <A
-        href="https://indiedao.gitbook.io/indiedao"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <StyledLinkText>Gitbook</StyledLinkText>
-      </A>
+        IndieDAO
+      </Link>
     </FooterLinksContainer>
     <Copyright>
       <Body1>Copyright © {new Date().getFullYear()} The IndieDAO</Body1>
     </Copyright>
   </FooterContainer>
 )
-
-Footer.propTypes = {}
-
-Footer.defaultProps = {}
 
 export default Footer
