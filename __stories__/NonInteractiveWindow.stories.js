4c211a3bffd402bff4c840dddb75c0d6570213b3
David McKelvey <david@mckelveycreative.co>
2022-01-15 16:38:29 -0800

    non interactive window tuned up

--- __stories__/NonInteractiveWindow.stories.js
+++ __stories__/NonInteractiveWindow.stories.js
@@ -1,43 +1,98 @@
-import styled from 'styled-components'
-
+import { COLOR_NAMES } from '../util/theme'
 import NonInteractiveWindowUI from '../components/ui/NonInteractiveWindow'
 
-const ShortContent = styled.div`
-  height: 50vh;
-`
-
-const LongContent = styled.div`
-  height: 150vh;
-`
-
 const Template = ({
-  'Tall Content': scrollbars,
-  Title,
-  'Title Bar Background Color': titleBarBackgroundColor,
-  'Content Background Color': contentBackgroundColor,
+  children,
+  contentBackgroundColor,
+  height,
+  title,
+  titleBarBackgroundColor,
+  width,
 }) => (
   <NonInteractiveWindowUI
-    title={Title}
-    titleBarBackgroundColor={titleBarBackgroundColor}
     contentBackgroundColor={contentBackgroundColor}
+    height={height}
+    title={title}
+    titleBarBackgroundColor={titleBarBackgroundColor}
+    width={width}
   >
-    {scrollbars ? <LongContent /> : <ShortContent />}
+    {children}
   </NonInteractiveWindowUI>
 )
 
 export const NonInteractiveWindow = Template.bind({})
 
 NonInteractiveWindow.args = {
-  Title: 'Skillz',
-  'Tall Content': false,
-  'Title Bar Background Color': 'vibrantBlue',
-  'Content Background Color': 'vibrantCream',
+  children: 'Content appears here.',
+  title: 'Skillz',
+  titleBarBackgroundColor: 'vibrantBlue',
+  contentBackgroundColor: 'vibrantCream',
+  width: 50,
+  height: 50,
 }
 
 const Story = {
   title: 'Design System / Non Interactive Window',
-  control: {
-    type: 'checkbox',
+  component: NonInteractiveWindowUI,
+  argTypes: {
+    titleBarBackgroundColor: {
+      description: 'The color of the title bar background.',
+      table: {
+        defaultValue: {
+          summary: 'vibrantBlue',
+        },
+      },
+      control: {
+        options: COLOR_NAMES,
+        type: 'select',
+      },
+    },
+    contentBackgroundColor: {
+      description: 'The color of the content background.',
+      table: {
+        defaultValue: {
+          summary: 'vibrantCream',
+        },
+      },
+      control: {
+        options: COLOR_NAMES,
+        type: 'select',
+      },
+    },
+    width: {
+      description: 'The width of the window.',
+      table: {
+        type: {
+          summary: '(in 10px rem)',
+        },
+        defaultValue: {
+          summary: 50,
+        },
+      },
+      control: {
+        type: 'range',
+        min: 1,
+        max: 100,
+        step: 0.1,
+      },
+    },
+    height: {
+      description: 'The height of the window.',
+      table: {
+        type: {
+          summary: '(in 10px rem)',
+        },
+        defaultValue: {
+          summary: 50,
+        },
+      },
+      control: {
+        type: 'range',
+        min: 1,
+        max: 100,
+        step: 0.1,
+      },
+    },
   },
 }
 
