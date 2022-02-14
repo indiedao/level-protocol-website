7a3fc0edd2613fc710c31eb1f86d52724d8178e4
nicovalencia <hello@nico.io>
2022-02-13 20:40:45 -0700

    Moves ens/address to Token. Adds Token Container.

--- components/token/ui/PFP.js
+++ components/token/ui/PFP.js
@@ -1,3 +1,9 @@
-const PFP = ({ src }) => <img src={src} />
+import styled from 'styled-components'
+
+const PFP = ({ src }) => <Img src={src} />
+
+const Img = styled.img`
+  height: 100%;
+`
 
 export default PFP
--- components/token/view/TokenView.js
+++ components/token/view/TokenView.js
@@ -1,16 +1,18 @@
 import Pyramid from '../../Pyramid'
 import useEns from '../../hooks/useEns'
 import PFP from '../ui/PFP'
+import TokenContainer from '../ui/TokenContainer'
+import { Body1 } from '../../ui/Typography'
 
 const TokenView = ({ address, nft }) => {
   const { ens } = useEns(address)
 
   return (
-    <div>
+    <TokenContainer width={300} height={400}>
       <PFP src={nft.src} />
-      {ens || address}
+      <Body1>{ens || address}</Body1>
       <Pyramid />
-    </div>
+    </TokenContainer>
   )
 }
 
