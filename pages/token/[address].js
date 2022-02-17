a13897630837d1f43c0c96b55e4977fce0253b2c
nicovalencia <hello@nico.io>
2022-02-17 00:16:17 -0700

    Adds more details to detail page.

--- pages/token/[address].js
+++ pages/token/[address].js
@@ -1,15 +1,24 @@
-import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { useEffect, useState, useCallback } from 'react'
 import Head from 'next/head'
 import { useRouter } from 'next/dist/client/router'
 import TokenView from '../../components/token/view/TokenView'
+import { Body1 } from '../../components/ui/Typography'
 
 const Level = () => {
   const [config, setConfig] = useState()
   const router = useRouter()
   const { address } = router.query
 
-  const loadConfig = async () => {
+  const loadConfig = useCallback(async () => {
     // TODO: load config from db:
+    const _config = await fetch(`/api/members/${address}/config`, {
+      method: 'GET',
+    })
+
+    console.log(_config)
+
+    // setConfig(_config)
 
     setConfig({
       address: '0x99b9bB23D300Dda4A1dC9eb8d2333704b235c165',
@@ -18,11 +27,13 @@ const Level = () => {
       nftAddress: '0xc0957ad0fc22ab988180772e96a8c29f2217e526',
       nftId: '346',
     })
-  }
+  }, [address])
 
   useEffect(() => {
     loadConfig()
-  }, [address])
+  }, [address, loadConfig])
+
+  if (!address) return <Body1>Loading...</Body1>
 
   return (
     <div>
@@ -31,18 +42,30 @@ const Level = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        {address && (
-          <TokenView
-            address={router.query.address}
-            nftId={config.nftId}
-            nftAddress={config.nftAddress}
-            colorHue={config.colorHue}
-            colorLightness={config.colorLightness}
-          />
-        )}
+        <Layout>
+          <Container>
+            {config && (
+              <TokenView
+                address={router.query.address}
+                nftId={config.nftId}
+                nftAddress={config.nftAddress}
+                colorHue={config.colorHue}
+                colorLightness={config.colorLightness}
+              />
+            )}
+          </Container>
+        </Layout>
       </main>
     </div>
   )
 }
+
+const Layout = styled.div``
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  max-width: 600px;
+`
 
 export default Level
