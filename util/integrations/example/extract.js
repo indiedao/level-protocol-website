75e4288c1f172ccd28d45624a88d9b1680f33d49
nicovalencia <hello@nico.io>
2022-02-26 16:58:38 -0700

    Adds ETL example files.

---
+++ util/integrations/example/extract.js
@@ -0,0 +1,8 @@
+export const extract = async () => {
+  // Fetch raw data from integration source:
+  // [...]
+  // TODO: add some examples using fetch/axios/graphql-request/etc.
+  const data = {}
+
+  return data
+}
