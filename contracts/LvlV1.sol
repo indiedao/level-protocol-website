809be33e11a9a2727c3387a24cf78050e62341a3
David W. McKelvey <david@mckelveycreative.co>
2022-03-07 16:23:44 -0800

    test fix (#79)

--- contracts/LvlV1.sol
+++ contracts/LvlV1.sol
@@ -1,4 +1,6 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
