8b0b7edaf435e19111b6cabdddbd36eab3f99eaa
David McKelvey <david@mckelveycreative.co>
2022-05-11 08:29:53 -0700

    PFP cleanup

--- components/token/ui/PFP.js
+++ components/token/ui/PFP.js
@@ -1,36 +1,51 @@
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
-const PFP = ({ src }) => (
-  <Wrapper>
-    <Img src={src} />
-    <Border />
-  </Wrapper>
-)
+const fallbackPixel =
+  'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAASABIAAD/2wCEAAEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAf/AABEIAAEAAQMBEQACEQEDEQH/xABLAAEAAAAAAAAAAAAAAAAAAAAHEAEAAAAAAAAAAAAAAAAAAAAAAQEBAAAAAAAAAAAAAAAAAAAFBxEBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEQMRAD8AWEfMP//Z'
 
-const Wrapper = styled.div`
-  height: 100%;
+const PFPFigure = styled.figure`
   position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: clamp(3.2rem, 8vh, 9.3rem);
+  width: clamp(3.2rem, 8vh, 9.3rem);
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJAAAACQAgMAAACaKAorAAAADFBMVEUAAAD//Nj//Nj//NhINzCmAAAAA3RSTlMA/gEHTjh7AAAAiUlEQVR4Xu3YMQqAQAxE0WVL7+f9T2EpESz8MEXYQhiFmS7h1UnIkNQdaVoQ4qFeBJHKjiAwM4JI04UwqowIo8qC1KjyIDWqgjCtCsJ06pMoCNOpF1FQUNC2/3VgBO1Z+SvoWD6ygno1axhQq+Y5bAilxoZQaiwIVUoKY0IwiB/xm5oFcSIcwoQuPyV9PGxoMVMAAAAASUVORK5CYII=');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: 100%;
+    z-index: 1000;
+  }
 `
 
 const Img = styled.img`
-  height: 100%;
-  max-height: 144px;
+  height: calc(100% - 0.3rem);
+  width: calc(100% - 0.3rem);
+  border-radius: 1000rem;
   object-fit: cover;
-  border-radius: 50%;
-  overflow: hidden;
-  padding: 2px;
 `
 
-const Border = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: url(/images/pixel-circle.png);
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-position: center;
-`
+const PFP = ({ alt, src }) => (
+  <PFPFigure>
+    <Img alt={alt} src={src || fallbackPixel} />
+  </PFPFigure>
+)
+
+PFP.propTypes = {
+  src: PropTypes.string,
+}
+
+PFP.defaultProps = {
+  src: undefined,
+}
 
 export default PFP
--- components/token/ui/PixelCard.js
+++ components/token/ui/PixelCard.js
@@ -1,40 +1,9 @@
 import styled, { css } from 'styled-components'
 
-// Construct a corner using a grid and css/div "Pixels":
-const Corner = ({ corner, color }) => (
-  <CornerGrid corner={corner}>
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <Pixel color={color} />
-    <div />
-    <Pixel color={color} />
-    <div />
-  </CornerGrid>
-)
-
-const PixelCard = ({ children, color }) => (
-  <OuterGrid>
-    <Corner corner={1} color={color} />
-    <Border side="top" color={color} />
-    <Corner corner={2} color={color} />
-    <Border side="left" color={color} />
-    <div>{children}</div>
-    <Border side="right" color={color} />
-    <Corner corner={3} color={color} />
-    <Border side="bottom" color={color} />
-    <Corner corner={4} color={color} />
-  </OuterGrid>
-)
-
 const OuterGrid = styled.div`
-  height: 100%;
-  width: 100%;
   display: grid;
-  grid-template-columns: 12px auto 12px;
-  grid-template-rows: 12px auto 12px;
+  grid-template-columns: 1.2rem 1fr 1.2rem;
+  grid-template-rows: 1.2rem 1fr 1.2rem;
 `
 
 const CornerGrid = styled.div`
@@ -69,5 +38,34 @@ const Border = styled.div`
     border-${props.side}: 4px solid ${props.color};
   `}
 `
+
+// Construct a corner using a grid and css/div "Pixels":
+const Corner = ({ corner, color }) => (
+  <CornerGrid corner={corner}>
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <Pixel color={color} />
+    <div />
+    <Pixel color={color} />
+    <div />
+  </CornerGrid>
+)
+
+const PixelCard = ({ children, color }) => (
+  <OuterGrid>
+    <Corner corner={1} color={color} />
+    <Border side="top" color={color} />
+    <Corner corner={2} color={color} />
+    <Border side="left" color={color} />
+    {children}
+    <Border side="right" color={color} />
+    <Corner corner={3} color={color} />
+    <Border side="bottom" color={color} />
+    <Corner corner={4} color={color} />
+  </OuterGrid>
+)
 
 export default PixelCard
